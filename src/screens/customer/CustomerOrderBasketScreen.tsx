4a5d71deda6ac6c22ea39507cc48fa5ddce390ea
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-09-26 10:35:54 +0500

    added selected product

--- src/screens/customer/CustomerOrderBasketScreen.tsx
+++ src/screens/customer/CustomerOrderBasketScreen.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 import Container from '../../components/common/Container/Container';
+import { useTypesSelector } from '../../app/store';
+import { selectedOrderProducts } from '../../app/services/order/orderSlice';
 
 const CustomerOrderBasketScreen = () => {
+  // Store
+  const selectedProduct = useTypesSelector(selectedOrderProducts);
+  
   return (
     <Container>
       <Text>CustomerOrderBasketScreen</Text>
