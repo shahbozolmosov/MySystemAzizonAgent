19c79cefde5b25dc0eb8175d0ddc742b16826735
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-09-27 10:13:18 +0500

    added EmptyBasket to CustomerOrderBasketScreen

--- src/screens/customer/CustomerOrderBasketScreen.tsx
+++ src/screens/customer/CustomerOrderBasketScreen.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 import {selectedOrderProducts} from '../../app/services/order/orderSlice';
 import {useTypesSelector} from '../../app/store';
 import BasketProductCardList from '../../components/common/BasketProductCard/BasketProductCardList';
 import Container from '../../components/common/Container/Container';
 import CustomerHeaderOperation from '../../components/customer/CustomerOperation/CustomerHeaderOperation';
+import EmptyBasket from '../../components/errors/EmptyBasket/EmptyBasket';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {CustomerTabStackParamList} from '../../routes/CustomerStack';
 
-const CustomerOrderBasketScreen = () => {
+type CustomerOrderBasketScreenProps =
+  NativeStackScreenProps<CustomerTabStackParamList>;
+
+const CustomerOrderBasketScreen = ({
+  navigation,
+}: CustomerOrderBasketScreenProps) => {
   // Store
   const selectedProduct = useTypesSelector(selectedOrderProducts);
+
+  const handleBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
 
   return (
     <Container>
       <CustomerHeaderOperation title="Mening savatim" showSearch />
-      <BasketProductCardList list={selectedProduct} />
+
+      {!selectedProduct.length ? (
+        <EmptyBasket onGoBack={handleBack} />
+      ) : (
+        <ScrollView>
+          <BasketProductCardList list={selectedProduct} />
+        </ScrollView>
+      )}
     </Container>
   );
 };
