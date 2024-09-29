bc9f6e016856d777944e1e60d354e40d7bdb822a
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-09-29 13:00:25 +0500

    create section title

---
+++ src/components/ui/SectionTitle/SectionTitle.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+type SectionTitleProps = {
+  title: string;
+};
+
+const SectionTitle = ({title}: SectionTitleProps) => {
+  return <Text style={styles.title}>{title}</Text>;
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Roboto-Back',
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#223263',
+    lineHeight: 40,
+  },
+});
+
+export default React.memo(SectionTitle);
