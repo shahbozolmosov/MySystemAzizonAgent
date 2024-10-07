b7e6c4d8114fb5b2bd2ecd266368b53809ab6d25
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-07 21:21:05 +0500

    remove clear btn

--- src/screens/main/HomeScreen.tsx
+++ src/screens/main/HomeScreen.tsx
@@ -1,14 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {Text, TouchableOpacity} from 'react-native';
 import Container from '../../components/common/Container/Container';
 import SyncBtn from '../../components/common/SyncBtn/SyncBtn.tsx';
 import {ICustomerCard} from '../../components/customer/CustomerCard/CustomerCard.tsx';
 import CustomerCardList from '../../components/customer/CustomerCard/CustomerCardList';
 import {getAllCustomers} from '../../database/customers.ts';
 import {getDBConnection} from '../../database/sqlite.ts';
-import {removeCustomersTable} from '../../database/tables/customers.table.ts';
-import {removeOrdersTable} from '../../database/tables/orders.table.ts';
-import {removeProductsTable} from '../../database/tables/product.table.ts';
 
 function HomeScreen() {
   // State
@@ -31,18 +27,8 @@ function HomeScreen() {
     initDB();
   }, []);
 
-  const handleClear = async () => {
-    const db = await getDBConnection();
-    await removeCustomersTable(db);
-    await removeProductsTable(db);
-    await removeOrdersTable(db);
-  };
-
   return (
     <Container>
-      <TouchableOpacity onPress={handleClear}>
-        <Text>Clear</Text>
-      </TouchableOpacity>
       <CustomerCardList list={customerData} />
 
       <SyncBtn />
