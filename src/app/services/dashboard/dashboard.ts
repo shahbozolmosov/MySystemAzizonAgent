ae67abee0a47463d69447642b251cc6c49034992
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-14 18:25:22 +0500

    create dashboard api

---
+++ src/app/services/dashboard/dashboard.ts
@@ -0,0 +1,38 @@
+import {allUrls} from '../../../constants/api';
+import {IApiRes} from '../../../types/api';
+import {apiSlice} from '../apiSlice.ts';
+
+// Show
+export interface DashboardItem {
+    id: 1;
+    name: string;
+    value: number;
+    icon_key: string;
+    status: string;
+}
+interface DashboardRes extends IApiRes {
+    data: DashboardItem;
+}
+
+const DASHBOARD_TAG = 'DASHBOARD_TAG';
+
+export const dashboardApi = apiSlice
+    .enhanceEndpoints({addTagTypes: [DASHBOARD_TAG]})
+    .injectEndpoints({
+        endpoints: build => ({
+            // Show
+            getDashboard: build.query<DashboardRes, void>({
+                query: () => allUrls.dashboardGet,
+                providesTags: [DASHBOARD_TAG],
+            }),
+            // Update
+            // Update password
+            // Logout
+        }),
+    });
+
+export const {useGetDashboardQuery} = dashboardApi;
+
+export const {
+    endpoints: {getDashboard},
+} = dashboardApi;
