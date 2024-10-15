13a3512dc6a0956988306c5b7d300a1fab3f8cb3
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-15 15:23:02 +0500

    refactored

--- src/screens/main/customer/AppCustomerDayScreen.tsx
+++ src/screens/main/customer/AppCustomerDayScreen.tsx
@@ -1,12 +1,11 @@
+import {MaterialTopTabScreenProps} from '@react-navigation/material-top-tabs';
 import React, {useEffect, useState} from 'react';
 import Container from '../../../components/common/Container/Container.tsx';
 import {ICustomerCard} from '../../../components/customer/CustomerCard/CustomerCard.tsx';
 import CustomerCardList from '../../../components/customer/CustomerCard/CustomerCardList';
 import {getAllCustomers} from '../../../database/customers.ts';
 import {getDBConnection} from '../../../database/sqlite.ts';
-import {MaterialTopTabScreenProps} from '@react-navigation/material-top-tabs';
 import {AppTabStackParamList} from '../../../routes/App/AppTabStack.tsx';
-import {Text} from 'react-native';
 
 type AppCustomerDayScreenProps = MaterialTopTabScreenProps<
     AppTabStackParamList,
