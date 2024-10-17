00ecb0f650f9dc452b2e845a556dfa80e3037d75
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-17 10:26:51 +0500

    refactored

--- src/screens/main/customer/AppCustomerDayScreen.tsx
+++ src/screens/main/customer/AppCustomerDayScreen.tsx
@@ -4,14 +4,13 @@ import {ICustomerCard} from '../../../components/customer/CustomerCard/CustomerC
 import CustomerCardList from '../../../components/customer/CustomerCard/CustomerCardList';
 import {getAllCustomers} from '../../../database/customers.ts';
 import {getDBConnection} from '../../../database/sqlite.ts';
-import {AppNativeStackParamList} from '../../../routes/App/AppNativeStack.tsx';
-import {Button} from '@rneui/themed';
-import {removeDayTable} from '../../../database/tables/day.table.ts';
-import {removeUserTable} from '../../../database/tables/user.table.ts';
 import {removeCustomersTable} from '../../../database/tables/customers.table.ts';
-import {removeProductsTable} from '../../../database/tables/product.table.ts';
-import {removeOrdersTable} from '../../../database/tables/orders.table.ts';
+import {removeDayTable} from '../../../database/tables/day.table.ts';
 import {removeOrdersDraftTable} from '../../../database/tables/orderDraft.table.ts';
+import {removeOrdersTable} from '../../../database/tables/orders.table.ts';
+import {removeProductsTable} from '../../../database/tables/product.table.ts';
+import {removeUserTable} from '../../../database/tables/user.table.ts';
+import {AppNativeStackParamList} from '../../../routes/App/AppNativeStack.tsx';
 
 type AppCustomerDayScreenProps = MaterialTopTabScreenProps<
     AppNativeStackParamList,
