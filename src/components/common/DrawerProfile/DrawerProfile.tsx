3d895f5c58bfb3ecd9c88cd222da506fcb998c3d
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-17 10:26:20 +0500

    change of drawer profile

--- src/components/common/DrawerProfile/DrawerProfile.tsx
+++ src/components/common/DrawerProfile/DrawerProfile.tsx
@@ -12,78 +12,80 @@ import {useDispatch} from 'react-redux';
 import {logout} from '../../../app/services/auth/authSlice.ts';
 
 type DrawerProfileNavigationProp = NativeStackNavigationProp<
-  AppRootDrawerStackParamList,
-  'Profile'
+    AppRootDrawerStackParamList,
+    'Profile'
 >;
 
 const DrawerProfile = () => {
-  const navigation = useNavigation<DrawerProfileNavigationProp>();
+    const navigation = useNavigation<DrawerProfileNavigationProp>();
 
-  const [user, setUser] = useState<IUser | null>(null);
+    const [user, setUser] = useState<IUser | null>(null);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    const initDB = async () => {
-      try {
-        const db = await getDBConnection();
-        const userData = await getUser(db);
-        if (!userData) {
-          dispatch(logout());
-        }
-        setUser(userData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+    useEffect(() => {
+        const initDB = async () => {
+            try {
+                const db = await getDBConnection();
+                const userData = await getUser(db);
+                if (!userData) {
+                    dispatch(logout());
+                }
+                setUser(userData);
+            } catch (err) {
+                console.error(err);
+            }
+        };
 
-    initDB();
-  }, [dispatch]);
+        initDB();
+    }, [dispatch]);
 
-  const handleNavigate = useCallback(() => {
-    navigation.navigate('Profile');
-  }, [navigation]);
+    const handleNavigate = useCallback(() => {
+        navigation.navigate('Profile');
+    }, [navigation]);
 
-  return (
-    <Pressable onPress={handleNavigate} style={styles.container}>
-      <Image source={ProfileImage} style={styles.profileImage} />
-      {user && (
-        <View>
-          <Text style={styles.name}>{`${user?.ism} ${user?.familya}`}</Text>
-          <Text style={styles.login}>{user?.login}</Text>
-        </View>
-      )}
-    </Pressable>
-  );
+    return (
+        <Pressable onPress={handleNavigate} style={styles.container}>
+            <Image source={ProfileImage} style={styles.profileImage} />
+            {user && (
+                <View>
+                    <Text
+                        style={
+                            styles.name
+                        }>{`${user?.ism} ${user?.familya}`}</Text>
+                    <Text style={styles.login}>{user?.login}</Text>
+                </View>
+            )}
+        </Pressable>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 30,
-    paddingHorizontal: 20,
-    backgroundColor: '#f4f4f4',
-    flexDirection: 'row',
-    gap: 20,
-    marginBottom: 20,
-  },
-  profileImage: {
-    width: 50,
-    height: 50,
-    borderRadius: 40,
-    marginBottom: 10,
-  },
-  name: {
-    fontFamily: 'Roboto-Black',
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: 4,
-  },
-  login: {
-    fontSize: 14,
-    fontWeight: 'light',
-    color: '#7e919a',
-  },
+    container: {
+        paddingVertical: 30,
+        paddingHorizontal: 20,
+        backgroundColor: '#f4f4f4',
+        flexDirection: 'row',
+        gap: 20,
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 40,
+        marginBottom: 10,
+    },
+    name: {
+        fontFamily: 'Roboto-Black',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom: 4,
+    },
+    login: {
+        fontSize: 14,
+        fontWeight: 'light',
+        color: '#7e919a',
+    },
 });
 
 export default React.memo(DrawerProfile);
